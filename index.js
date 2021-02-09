2b8bbe4a52f5dd135b51e7b8bee973880781b331
jmd <jordan.michael.dickey@gmail.com>
2021-02-08 20:51:41 -0700

    commit tree 537b6eeebf334fc4b0d6e1f9820dc5066d98e430

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+class Cat {
+    constructor(name,sex){
+       this.name = name
+       this.sex = sex 
+    }
+
+     speak() {
+        return `${this.name} says meow!`
+    }
+  }
+   
+  class Dog {
+    constructor(name,sex){
+        this.name = name
+        this.sex = sex 
+     }
+
+     speak() {
+        return `${this.name} says woof!`
+    }
+  }
+   
+  class Bird {
+    constructor(name,sex){
+        this.name = name
+        this.sex = sex 
+     }
+     speak() {
+        // let male = `It's me! ${this.name}, the parrot!`
+        // let female = `${this.name} says squawk!`
+        return  this.sex === "male" ?  `It's me! ${this.name}, the parrot!` : `${this.name} says squawk!`
+    //     if (this.sex === "male") {
+    //         return `It's me! ${this.name}, the parrot!`
+    // }else {
+    //     return `${this.name} says squawk!`
+    // }
+  }
+}
